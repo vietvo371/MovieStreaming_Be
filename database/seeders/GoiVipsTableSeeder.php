ee94b3d47f9e34383db025e64492703f3491369b
Văn Việt <vietvo371@gmail.com>
2024-12-18 17:15:25 +0700

    add chatbot

--- database/seeders/GoiVipsTableSeeder.php
+++ database/seeders/GoiVipsTableSeeder.php
@@ -19,24 +19,24 @@ class GoiVipsTableSeeder extends Seeder
                 'ten_goi' => 'Gói 1 Tháng',
                 'slug_goi_vip' => 'goi-1-thang',
                 'thoi_han' => 1, // Duration in months
-                'tien_goc' => 200000, // Original price
-                'tien_sale' => 180000, // Discounted price
+                'tien_goc' => 25000, // Original price
+                'tien_sale' => 20000, // Discounted price
                 'tinh_trang' => 1, // Active status
             ],
             [
                 'ten_goi' => 'Gói 3 Tháng',
                 'slug_goi_vip' => 'goi-3-thang',
                 'thoi_han' => 3, // Duration in months
-                'tien_goc' => 500000, // Original price
-                'tien_sale' => 450000, // Discounted price
+                'tien_goc' => 75000, // Original price
+                'tien_sale' => 60000, // Discounted price
                 'tinh_trang' => 1, // Active status
             ],
             [
                 'ten_goi' => 'Gói 6 Tháng',
                 'slug_goi_vip' => 'goi-6-thang',
                 'thoi_han' => 6, // Duration in months
-                'tien_goc' => 1000000, // Original price
-                'tien_sale' => 850000, // Discounted price
+                'tien_goc' => 150000, // Original price
+                'tien_sale' => 120000, // Discounted price
                 'tinh_trang' => 1, // Active status
             ],
             // [
